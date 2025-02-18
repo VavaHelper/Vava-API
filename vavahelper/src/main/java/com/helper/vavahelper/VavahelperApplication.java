fdb87d7aaa201ca95a0523b2283ec6b36d298e38
Nadruz <juliananadruz@gmail.com>
2025-02-17 21:14:16 -0300

    Inicializando projeto!

---
+++ vavahelper/src/main/java/com/helper/vavahelper/VavahelperApplication.java
@@ -0,0 +1,13 @@
+package com.helper.vavahelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VavahelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VavahelperApplication.class, args);
+	}
+
+}
