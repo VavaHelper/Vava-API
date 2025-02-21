bd4f391cd8244ee9bcbc73cf5119d16ed1d8589e
Nadruz <juliananadruz@gmail.com>
2025-02-21 18:43:30 -0300

    Finalizando primeira demanda!

--- src/main/java/com/helper/vavahelper/Models/Agents.java
+++ src/main/java/com/helper/vavahelper/Models/Agents.java
@@ -1,5 +1,25 @@
 package com.helper.vavahelper.Models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Agents")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Agents {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", unique = true, nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "ult_points", nullable = false)
+    private int ultPoints;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
 }
--- src/main/java/com/helper/vavahelper/Repositories/AgentsRepository.java
+++ src/main/java/com/helper/vavahelper/Repositories/AgentsRepository.java
@@ -1,5 +1,9 @@
 package com.helper.vavahelper.Repositories;
 
-public interface AgentsRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.helper.vavahelper.Models.Agents;
+
+public interface AgentsRepository extends JpaRepository<Agents, Integer>{
     
 }
