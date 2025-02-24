b7dfd3011920c882df6d43533126271a4502c3bc
André Felipe de Barros A. N. <100530782+andrefelipebarros@users.noreply.github.com>
2025-02-24 11:46:06 -0300

    chore: Configuração do banco H2 finalizado.

--- src/main/java/com/helper/vavahelper/Config/SecurityConfig.java
+++ src/main/java/com/helper/vavahelper/Config/SecurityConfig.java
@@ -4,9 +4,11 @@ import com.helper.vavahelper.Repositories.UserRepository;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -26,5 +28,20 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    //Filtro de configuração para utilizar o H2
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/h2/*").hasRole("ADMIN") // Apenas ADMIN pode acessar o H2.
+                .anyRequest().authenticated()
+            )
+            .csrf(csrf -> csrf.ignoringRequestMatchers("/h2/*")) // Desativa CSRF ao H2.
+            .headers(headers -> headers.frameOptions().disable())
+            .formLogin(); // Tela de login do Spring Security.
+
+        return http.build();
     }
 }
