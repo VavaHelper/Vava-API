d593dafa49f835ea106637ae096015fcb5a0ed01
Matheus Rocha <matheusrmds@gmail.com>
2025-03-01 19:42:57 -0300

    #Skills - Models and Repositories

---
+++ src/main/java/com/helper/vavahelper/models/Skills/Skills.java
@@ -0,0 +1,27 @@
+package com.helper.vavahelper.models.Skills;
+
+import com.helper.vavahelper.models.Agents.Agents;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Skills")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Skills {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @ManyToOne
+    @JoinColumn(name = "agente_id", nullable = false)
+    private Agents agent;
+    
+    @Column(name = "nome", nullable = false, length = 50)
+    private String name;
+    
+    @Column(name = "descricao", columnDefinition = "TEXT")
+    private String description;
+}
