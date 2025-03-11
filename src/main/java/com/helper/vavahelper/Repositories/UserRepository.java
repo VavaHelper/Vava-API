d8e8dace42a66876a0bfe8a13f32f99578737c56
André Felipe de Barros A. N. <100530782+andrefelipebarros@users.noreply.github.com>
2025-03-11 19:56:27 -0300

    refactor: Update UserRepository.java

--- src/main/java/com/helper/vavahelper/Repositories/UserRepository.java
+++ src/main/java/com/helper/vavahelper/Repositories/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import com.helper.vavahelper.models.User.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
     UserDetails findByLogin(String login);
 }
