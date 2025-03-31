0454fcb855f7d859c4004bc0c30afdedf257aa77
Andre Felipe B. A. Nogueira <afbarrosan@gmail.com>
2025-03-31 13:44:02 -0300

    feat: Create new Collumn with LocalDateTime

--- src/main/java/com/helper/vavahelper/models/User/User.java
+++ src/main/java/com/helper/vavahelper/models/User/User.java
@@ -1,35 +1,46 @@
 package com.helper.vavahelper.models.User;
+
+
 import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
 @Data
-@Setter
-@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name= "users")
 @Entity(name = "users")
 @EqualsAndHashCode(of = "id")
 public class User implements UserDetails {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
+    @Column(nullable = false)
     private UserRole role;
-    @Column(unique=true)
+
+    @Column(unique = true, nullable = false)
     private String login;
+
+    @Column(nullable = false)
     private String password;
+    
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
     
     //Constructor
     public User(String login, String password, UserRole role){
