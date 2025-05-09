5b0a075a76b3af3a388935c9f58dd1942f820f07
Andre Felipe de Barros Azevedo Nogueira <andre.nogueira@bndes.gov.br>
2025-05-09 13:03:30 -0300

    chore: remover diretório duplicado Repositories

--- src/main/java/com/helper/vavahelper/Repositories/LineupsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.helper.vavahelper.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.helper.vavahelper.models.Lineups.Lineups;
-
-public interface LineupsRepository extends JpaRepository<Lineups, Long> {
-    
-}
